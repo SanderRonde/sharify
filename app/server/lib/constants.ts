2313b53279440127f29f1e921aec3e7e4ff9dfe4
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-03-02 03:26:56 +0100

    remove TODO

--- app/server/lib/constants.ts
+++ app/server/lib/constants.ts
@@ -7,7 +7,6 @@ export const ROOM_ID_LENGTH = 32;
 // Time out after a week
 export const ROOM_TIMEOUT = 1000 * 60 * 60 * 24 * 7;
 
-// TODO: get the relevant scopes
 export const SPOTIFY_PEER_SCOPES = ["user-read-email", "user-top-read"];
 export const SPOTIFY_HOST_SCOPES = [
     ...SPOTIFY_PEER_SCOPES,
