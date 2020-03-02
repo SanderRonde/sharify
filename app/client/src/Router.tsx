6e0c3e999b6614e0cd7a508cdcf6cb5ef7d238b8
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-03-02 05:12:56 +0100

    use router

--- app/client/src/Router.tsx
+++ app/client/src/Router.tsx
@@ -1,15 +1,15 @@
-// import styled from 'styled-components';
-// import { DatePicker } from 'antd';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './home/Home';
+import Home from './components/home/Home';
 import React from "react";
-import "./App.css";
 
 export default class AppRouter extends React.Component {
 	render() {
 		return (
 			<Router>
 				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/new_room">
 					<Home />
 				</Route>
 			</Router>
