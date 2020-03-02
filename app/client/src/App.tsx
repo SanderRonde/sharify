05c4ee1217c1b1eef5cc63a822b6b61b7ad95798
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-03-02 04:29:03 +0100

    set up router

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import styled from 'styled-components';
+// import { DatePicker } from 'antd';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './home/Home';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class AppRouter extends React.Component {
+	render() {
+		return (
+			<Router>
+				<Route exact path="/">
+					<Home />
+				</Route>
+			</Router>
+		)
+	}
 }
-
-export default App;
---
+++ app/client/src/home/Home.tsx
@@ -0,0 +1,19 @@
+// import styled from 'styled-components';
+// import { DatePicker } from 'antd';
+import React from "react";
+import "./App.css";
+
+export default class Home extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src="logo512.png" className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.tsx</code> and save to reload.
+					</p>
+				</header>
+			</div>
+		);
+	}
+}
