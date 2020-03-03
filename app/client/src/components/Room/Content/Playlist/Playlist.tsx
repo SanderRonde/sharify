3d372f703a8bf6a54d00780b6f1551804c1675c8
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-03-03 05:22:03 +0100

    add delay while loading

--- app/client/src/components/Room/Content/Playlist/Playlist.tsx
+++ app/client/src/components/Room/Content/Playlist/Playlist.tsx
@@ -1,26 +1,33 @@
-import { Verticalcenterer } from "../../../shared/styles";
-import { Spin } from "antd";
-import React from "react";
+import { Verticalcenterer } from '../../../shared/styles';
+import { useState } from 'react';
+import { Spin } from 'antd';
+import React from 'react';
 
 export default function Playlist({ id }: { id: string | null }) {
+    const [isLoading, setLoading] = useState<boolean>(true);
+
     if (!id) {
-        // TODO: Loading screen
         return (
             <Verticalcenterer>
-                <Spin style={{ width: "100%" }} size="large" />
+                <Spin style={{ width: '100%' }} size="large" />
             </Verticalcenterer>
         );
     }
 
     return (
-        <iframe
-            title="spotify-playlist"
-            src={`https://open.spotify.com/embed/playlist/${id}`}
-            style={{ minHeight: "70vh", width: "100%" }}
-            //@ts-ignore
-            frameborder="0"
-            allowtransparency="true"
-            allow="encrypted-media"
-        ></iframe>
+        <Verticalcenterer>
+            <Spin style={{ width: '100%' }} size="large" spinning={isLoading} delay={500}>
+                <iframe
+                    title="spotify-playlist"
+                    src={`https://open.spotify.com/embed/playlist/${id}`}
+                    style={{ minHeight: '70vh', width: '100%' }}
+                    frameBorder="0"
+                    //@ts-ignore
+                    allowtransparency="true"
+                    allow="encrypted-media"
+                    onLoad={() => setLoading(false)}
+                ></iframe>
+            </Spin>
+        </Verticalcenterer>
     );
 }
