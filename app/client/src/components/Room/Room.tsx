c43b362a77e7983a4c9741e0c6e68a759f7f1f3f
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-03-03 06:36:16 +0100

    add a debug state

--- app/client/src/components/Room/Room.tsx
+++ app/client/src/components/Room/Room.tsx
@@ -1,12 +1,60 @@
-import { Background } from "../shared/styles";
-import Content from "./Content/Content";
-import Header from "./Header/Header";
-import { Page } from "./styles";
-import React from "react";
+import { Background } from '../shared/styles';
+import Content from './Content/Content';
+import Header from './Header/Header';
+import { Page } from './styles';
+import React from 'react';
 
 const DEBUG = true;
+const DEBUG_HOST = true;
+const DEFAULT_STATE = {
+    playlistID: '1lJr8Bej7l1x69s582H4wo',
+    members: [
+        {
+            id: 'something',
+            isAdmin: false,
+            isMe: false,
+            image: null,
+            name: 'Sander Normal',
+            email: 'awsdfgvhbjn@gmail.com',
+        },
+        {
+            id: 'something1',
+            isAdmin: true,
+            isMe: false,
+            image: null,
+            name: 'Sander Host',
+            email: 'awsdfgvhbjn@gmail.com',
+        },
+        {
+            id: 'something2',
+            isHost: DEBUG_HOST,
+            isAdmin: true,
+            isMe: true,
+            image: null,
+            name: 'Sander Me',
+            email: 'awsdfgvhbjn@gmail.com',
+        },
+        {
+            id: 'something3',
+            isAdmin: false,
+            isMe: false,
+            image:
+                'https://help.seesaw.me/hc/en-us/article_attachments/203872726/cow.png',
+            name: 'Sander Image',
+            email: 'awsdfgvhbjn@gmail.com',
+        },
+    ] as RoomMember[],
+};
 
-export interface RoomMember {}
+export interface RoomMember {
+    id: string;
+    isHost: boolean;
+    isAdmin: boolean;
+    isMe: boolean;
+    image: string | null;
+    name: string;
+    email: string;
+}
 
 interface Props {
     id: string;
@@ -19,14 +67,12 @@ class Room extends React.Component<
         members: RoomMember[];
     }
 > {
-    constructor(props: Props, context?: any) {
-        super(props, context);
-
-        this.state = {
-            playlistID: DEBUG ? '1lJr8Bej7l1x69s582H4wo' : null,
-            members: [],
-        };
-    }
+    state = DEBUG
+        ? DEFAULT_STATE
+        : {
+              playlistID: null,
+              members: [],
+          };
 
     render() {
         return (
