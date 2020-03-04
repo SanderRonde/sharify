745339960be72288914cda01a1a431ab94a295b4
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-03-04 21:58:18 +0100

    make 404 default route

--- app/client/src/Router.tsx
+++ app/client/src/Router.tsx
@@ -1,6 +1,6 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Rejected from './components/SingleMessagePages/Rejected/Rejected';
 import FiveHundred from './components/SingleMessagePages/500/500';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import FourOFour from './components/SingleMessagePages/404/404';
 import Home from './components/SingleMessagePages/Home/Home';
 import RoomWrapper from './components/Room/Wrapper';
@@ -10,21 +10,23 @@ export default class AppRouter extends React.Component {
     render() {
         return (
             <Router>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                <Route path="/room/:roomID">
-                    <RoomWrapper />
-                </Route>
-                <Route path="/404">
-                    <FourOFour />
-                </Route>
-                <Route path="/500">
-                    <FiveHundred />
-                </Route>
-                <Route path="/rejected/:url">
-                    <Rejected />
-                </Route>
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/room/:roomID">
+                        <RoomWrapper />
+                    </Route>
+                    <Route path="/500">
+                        <FiveHundred />
+                    </Route>
+                    <Route path="/rejected/:url">
+                        <Rejected />
+                    </Route>
+                    <Route path="*">
+                        <FourOFour />
+                    </Route>
+                </Switch>
             </Router>
         );
     }
