f43bb96d16019120855753b61d70c4f07b70c6b4
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-03-04 01:25:16 +0100

    scale margins based on device size

--- app/client/src/components/Room/styles.ts
+++ app/client/src/components/Room/styles.ts
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const Page = styled.div`
 	max-width: 1000px;
-	margin-left: 20px;
-	margin-right: 20px;
+	margin-left: 5vw;
+	margin-right: 5vw;
 	width: 100%;
 	display: flex;
 	flex-direction: column;
